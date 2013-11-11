c8b55869c505ce95217c44b92ee7bc0f20bbeda1
Witold <wewolski@gmail.com>
2013-11-11 23:56:47 +0100

    [BUILD]

---
+++ cmake_modules/projects.cmake
@@ -0,0 +1,8 @@
+unset(projects)
+
+# gtest
+list(APPEND projects gtest)
+set(gtest_version "1.6.0")
+set(gtest_url "http://googletest.googlecode.com/files/gtest-${gtest_version}.zip")
+set(gtest_md5 "4577b49f2973c90bf9ba69aa8166b786")
+
